3efae50ce180d93cdcf0a4abd13681921cbe0075
Keyur Patel <patel2@mobifyi.com>
2019-11-04 12:39:10 +0530

    test project for Travis CI

---
+++ Base/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Base
+//
+//  Created by Keyur Patel on 03/11/19.
+//  Copyright © 2019 3Embed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let label = UILabel.init()
+        label.text = "Welcome"
+        view.addSubview(label)
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BaseUITests/BaseUITests.swift
@@ -0,0 +1,28 @@
+//
+//  BaseUITests.swift
+//  BaseUITests
+//
+//  Created by Keyur Patel on 03/11/19.
+//  Copyright © 2019 3Embed. All rights reserved.
+//
+
+import XCTest
+
+class BaseUITests: XCTestCase {
+    var app: XCUIApplication!
+    override func setUp() {
+        continueAfterFailure = false
+        app = XCUIApplication()
+        app.launch()
+
+    }
+
+    func testExample() {
+        // given
+        let label1 = app.staticTexts["Hello"]
+        
+        // then
+        XCTAssertTrue(label1.exists)
+    }
+
+}
